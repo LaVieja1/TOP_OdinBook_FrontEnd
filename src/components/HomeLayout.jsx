86a31e103e6328d1ce111d2c4c90d9601b0fe707
santicano <canosantiago404@gmail.com>
2024-01-21 17:25:43 -0300

    added Login page to App

---
+++ src/components/HomeLayout.jsx
@@ -0,0 +1,8 @@
+import { Navigate, Outlet  } from 'react-router-dom';
+import Cookies from 'js-cookie';
+
+export const HomeLayout = () => {
+    const user = Cookies.get('jwt_token');
+
+    return user ? <Navigate to="/home" /> : <Outlet />;
+};
