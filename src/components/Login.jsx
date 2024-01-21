2414e9b65d0f29cff0f6227fe312605e77ec6637
santicano <canosantiago404@gmail.com>
2024-01-21 17:19:18 -0300

    added Login component

---
+++ src/components/Login.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import LoginForm from './LoginForm';
+
+function LogIn() {
+    const [showSignUpForm, setShowSignUpForm] = useState(false);
+    const [slideUp, setSlideUp] = useState(false);
+
+    const handleButtonClick = () => {
+        setSlideUp(true);
+        setTimeout(() => {
+            setShowSignUpForm(true);
+        }, '500');
+    };
+
+    return (
+        <div className="h-screen w-screen flex justify-center items-center bg-gradient-to-r from-white to-blue-400">
+            <div>
+                {showSignUpForm ? (
+                    <LoginForm />
+                ) : (
+                    <button
+                        className={`py-14 px-5 bg-transparent transition-all duration-1000 hover:bg-blue-400 hover:rounded-2xl ${slideUp ? "-translate-y-full transition-transform ease-in-out duration-500" : ''}`}
+                        onClick={handleButtonClick}
+                    >
+                        <h1>OdinBook</h1>
+                    </button>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default LogIn;
