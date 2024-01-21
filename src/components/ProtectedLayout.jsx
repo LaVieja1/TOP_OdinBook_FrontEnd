d09d4b3a5ae0a43aa3d03315df1748adb4752e19
santicano <canosantiago404@gmail.com>
2024-01-21 20:37:02 -0300

    added ProtectedLayout component

---
+++ src/components/ProtectedLayout.jsx
@@ -0,0 +1,20 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import Nav from './Nav';
+import { useState } from 'react';
+
+export const ProtectedLayout = () => {
+    const [updateUser, setUpdateUser] = useState(false);
+    const user = Cookies.get('jwt_token');
+
+    if (!user) {
+        return <Navigate to="/login" />;
+    }
+
+    return (
+        <>
+            <Nav updateUser={updateUser} setUpdateUser={setUpdateUser} />
+            <Outlet context={[updateUser, setUpdateUser]} />
+        </>
+    );
+};
